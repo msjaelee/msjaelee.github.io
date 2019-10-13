ddc922995de591810b8e3621a4d18385b802f04b
jaelee <jls916@gmail.com>
2019-10-12 18:58:36 -0600

    Fixed bug in date algorithm.
    Fixed bug that wasn't allowing assignments to update.
    Added holiday cards.
    Fixed dates to include holidays with no assignments.

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function weeklyAssignment() {
     pdfSrc = `"Step2Week13.pdf"`;
   } else if (
     (Month == 8 && (Today >= 25 && Today <= 30)) ||
-    (Month == 9 && Today >= 1)
+    (Month == 9 && Today == 1)
   ) {
     weekNumber = 14;
     stepNumber = 2;
@@ -109,7 +109,7 @@ function weeklyAssignment() {
     pdfSrc = `"Step3Week18.pdf"`;
   } else if (
     (Month == 9 && (Today >= 30 && Today <= 31)) ||
-    (Month == 10 && (Today >= 1) & (Today <= 5))
+    (Month == 10 && (Today >= 1 && Today <= 5))
   ) {
     weekNumber = 19;
     stepNumber = 3;
@@ -127,132 +127,152 @@ function weeklyAssignment() {
     stepNumber = 4;
     pdfSrc = `"Step4Week22.pdf"`;
     addButton = `"Step4SuppReading.pdf"`;
-  } else if (
-    (Month == 10 && (Today >= 27 && Today <= 30)) ||
-    (Month == 11 && (Today >= 1 && Today <= 3))
-  ) {
+  } else if (Month == 10 && (Today >= 27 && Today <= 30)) {
+    weekNumber = 22;
+    stepNumber = 4;
+    pdfSrc = `"Step4Week22.pdf"`;
+    addButton = `"thanksgivingCard.pdf"`;
+  } else if (Month == 11 && (Today >= 1 && Today <= 3)) {
+    weekNumber = 22;
+    stepNumber = 4;
+    pdfSrc = `"Step4Week22.pdf"`;
+    addButton = `"Step4SuppReading.pdf"`;
+  } else if (Month == 11 && (Today >= 4 && Today <= 10)) {
     weekNumber = 23;
     stepNumber = 4;
     pdfSrc = `"Step4Week23.pdf"`;
     addButton = `"Step4SuppReading.pdf"`;
-  } else if (Month == 11 && (Today >= 4 && Today <= 10)) {
+  } else if (Month == 11 && (Today >= 11 && Today <= 17)) {
     weekNumber = 24;
     stepNumber = 4;
     pdfSrc = `"Step4Week24.pdf"`;
-  } else if (Month == 11 && (Today >= 11 && Today <= 17)) {
+  } else if (Month == 11 && (Today >= 18 && Today <= 24)) {
+    weekNumber = 25;
+    stepNumber = 4;
+    pdfSrc = `"Step4Week25.pdf"`;
+    addButton = `"holidayCard.pdf"`;
+  } else if (Month == 11 && (Today >= 25 && Today <= 31)) {
     weekNumber = 25;
     stepNumber = 4;
     pdfSrc = `"Step4Week25.pdf"`;
     addButton = `"Step4SuppReading.pdf"`;
-  } else if (Month == 11 && (Today >= 18 && Today <= 24)) {
+  } else if (Month == 0 && (Today >= 1 && Today <= 7)) {
+    weekNumber = 25;
+    stepNumber = 4;
+    pdfSrc = `"Step4Week25.pdf"`;
+    addButton = `"newyearsCard.pdf"`;
+  } else if (Month == 11 && (Today >= 8 && Today <= 14)) {
     weekNumber = 26;
     stepNumber = 4;
     pdfSrc = `"Step4Week26.pdf"`;
     addButton = `"Step4SuppReading.pdf"`;
-  } else if (Month == 11 && (Today >= 25 && Today <= 31)) {
+  } else if (Month == 11 && (Today >= 15 && Today <= 21)) {
     weekNumber = 27;
     stepNumber = 4;
     pdfSrc = `"Step4Week27.pdf"`;
     addButton = `"Step4SuppReading.pdf"`;
-  } else if (Month == 0 && (Today >= 1 && Today <= 7)) {
+  } else if (Month == 0 && (Today >= 22 && Today <= 28)) {
     weekNumber = 28;
     stepNumber = 4;
     pdfSrc = `"Step4Week28.pdf"`;
     addButton = `"Step4SuppReading.pdf"`;
-  } else if (Month == 0 && (Today >= 8 && Today <= 14)) {
-    weekNumber = 29;
-    stepNumber = 4;
-    pdfSrc = `"Step4Week29.pdf"`;
-    addButton = `"Step4SuppReading.pdf"`;
-  } else if (Month == 0 && (Today >= 15 && Today <= 21)) {
-    weekNumber = 30;
-    stepNumber = 4;
-    pdfSrc = `"Step4Week30.pdf"`;
-    addButton = `"Step4SuppReading.pdf"`;
-  } else if (Month == 0 && (Today >= 22 && Today <= 28)) {
-    weekNumber = 31;
-    stepNumber = 4;
-    pdfSrc = `"Step4Week31.pdf"`;
-    addButton = `"Step5PrepWeek31.pdf"`;
   } else if (
     (Month == 0 && (Today >= 29 && Today <= 31)) ||
     (Month == 1 && (Today >= 1 && Today <= 4))
   ) {
+    weekNumber = 29;
+    stepNumber = 4;
+    pdfSrc = `"Step4Week29.pdf"`;
+    addButton = `"Step4SuppReading.pdf"`;
+  } else if (Month == 1 && (Today >= 5 && Today <= 11)) {
+    weekNumber = 30;
+    stepNumber = 4;
+    pdfSrc = `"Step4Week30.pdf"`;
+    addButton = `"Step4SuppReading.pdf"`;
+  } else if (Month == 1 && (Today >= 12 && Today <= 18)) {
+    weekNumber = 31;
+    stepNumber = 4;
+    pdfSrc = `"Step4Week31.pdf"`;
+    addButton = `"Step5PrepWeek31.pdf"`;
+  } else if (Month == 1 && (Today >= 19 && Today <= 25)) {
     weekNumber = 32;
     stepNumber = 4;
     pdfSrc = `"Step4Week32.pdf"`;
     addButton = `"Step5PrepWeek31.pdf"`;
-  } else if (Month == 1 && (Today >= 5 && Today <= 11)) {
-    weekNumber = 33;
-    stepNumber = 5;
-    pdfSrc = `"Step5Week33.pdf"`;
-  } else if (Month == 1 && (Today >= 12 && Today <= 18)) {
-    weekNumber = 34;
-    stepNumber = 5;
-    pdfSrc = `"Step5Week34.pdf"`;
-  } else if (Month == 1 && (Today >= 19 && Today <= 25)) {
-    weekNumber = 35;
-    stepNumber = 6;
-    pdfSrc = `"Step6Week35.pdf"`;
   } else if (
     (Month == 1 && (Today >= 26 && Today <= 29)) ||
     (Month == 2 && (Today >= 1 && Today <= 3))
   ) {
+    weekNumber = 33;
+    stepNumber = 5;
+    pdfSrc = `"Step5Week33.pdf"`;
+  } else if (Month == 2 && (Today >= 4 && Today <= 10)) {
+    weekNumber = 34;
+    stepNumber = 5;
+    pdfSrc = `"Step5Week34.pdf"`;
+  } else if (Month == 2 && (Today >= 11 && Today <= 17)) {
+    weekNumber = 35;
+    stepNumber = 6;
+    pdfSrc = `"Step6Week35.pdf"`;
+  } else if (Month == 2 && (Today >= 18 && Today <= 24)) {
     weekNumber = 36;
     stepNumber = 6;
     pdfSrc = `"Step6Week36.pdf"`;
-  } else if (Month == 2 && (Today >= 4 && Today <= 10)) {
+  } else if (Month == 2 && (Today >= 25 && Today <= 31)) {
     weekNumber = 37;
     stepNumber = 7;
     pdfSrc = `"Step7Week37.pdf"`;
-  } else if (Month == 2 && (Today >= 4 && Today <= 10)) {
+  } else if (Month == 3 && (Today >= 1 && Today <= 7)) {
     weekNumber = 38;
     stepNumber = 7;
     pdfSrc = `"Step7Week38.pdf"`;
-  } else if (Month == 2 && (Today >= 11 && Today <= 17)) {
+  } else if (Month == 3 && (Today >= 8 && Today <= 14)) {
     weekNumber = 39;
     stepNumber = 8;
     pdfSrc = `"Step8Week39.pdf"`;
-  } else if (Month == 2 && (Today >= 18 && Today <= 24)) {
+  } else if (Month == 3 && (Today >= 15 && Today <= 21)) {
     weekNumber = 40;
     stepNumber = 8;
     pdfSrc = `"Step8Week40.pdf"`;
-  } else if (Month == 2 && (Today >= 25 && Today <= 31)) {
+  } else if (Month == 3 && (Today >= 22 && Today <= 28)) {
     weekNumber = 41;
     stepNumber = 9;
     pdfSrc = `"Step9Week41.pdf"`;
-  } else if (Month == 3 && (Today >= 1 && Today <= 7)) {
-    weekNumber = 42;
-    stepNumber = 9;
-    pdfSrc = `"Step9Week42.pdf"`;
-  } else if (Month == 3 && (Today >= 8 && Today <= 14)) {
-    weekNumber = 43;
-    stepNumber = 10;
-    pdfSrc = `"Step10Week43.pdf"`;
-  } else if (Month == 3 && (Today >= 15 && Today <= 21)) {
-    weekNumber = 44;
-    stepNumber = 10;
-    pdfSrc = `"Step10Week44.pdf"`;
-  } else if (Month == 3 && (Today >= 22 && Today <= 28)) {
-    weekNumber = 45;
-    stepNumber = 10;
-    pdfSrc = `"Step10Week45.pdf"`;
   } else if (
     (Month == 3 && (Today >= 29 && Today <= 30)) ||
     (Month == 4 && (Today >= 1 && Today <= 5))
   ) {
+    weekNumber = 42;
+    stepNumber = 9;
+    pdfSrc = `"Step9Week42.pdf"`;
+  } else if (Month == 4 && (Today >= 6 && Today <= 12)) {
+    weekNumber = 43;
+    stepNumber = 10;
+    pdfSrc = `"Step10Week43.pdf"`;
+  } else if (Month == 4 && (Today >= 13 && Today <= 19)) {
+    weekNumber = 44;
+    stepNumber = 10;
+    pdfSrc = `"Step10Week44.pdf"`;
+  } else if (Month == 4 && (Today >= 20 && Today <= 26)) {
+    weekNumber = 45;
+    stepNumber = 10;
+    pdfSrc = `"Step10Week45.pdf"`;
+  } else if (
+    (Month == 4 && (Today >= 27 && Today <= 31)) ||
+    (Month == 5 && (Today >= 1 && Today <= 2))
+  ) {
     weekNumber = 46;
     stepNumber = 10;
     pdfSrc = `"Step10Week46.pdf"`;
-  } else if (Month == 4 && (Today >= 6 && Today <= 12)) {
+  } else if (Month == 5 && (Today >= 3 && Today <= 9)) {
     weekNumber = 47;
     stepNumber = 11;
     pdfSrc = `"Step11Week47.pdf"`;
-  } else if (Month == 4 && (Today >= 13 && Today <= 19)) {
+  } else if (Month == 5 && (Today >= 10 && Today <= 16)) {
     weekNumber = 48;
     stepNumber = 11;
     pdfSrc = `"Step11Week48.pdf"`;
-  } else if (Month == 4 && (Today >= 20 && Today <= 26)) {
+  } else if (Month == 5 && (Today >= 17 && Today <= 23)) {
     weekNumber = 49;
     stepNumber = 12;
     pdfSrc = `"Step12Week49.pdf"`;
